10f9f8b368a02699e0145315b51270c95f86cfcd
Jeremias Erbs <jerry@badabam.de>
2021-10-18 12:10:21 +0200

    Add nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { getGreeting } from './getGreeting.js'
 import { replaceName } from './replaceName.js'
 
-console.log(getGreeting('Jane Doe'))
+console.log(getGreeting('Raphael'))
 
 console.log(replaceName('Hello Jane', 'Jane', 'John'))
 console.log(replaceName('Hello Jane. Goodbye Jane.', 'Jane', 'John'))
